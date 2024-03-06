6ae8969f2f0c5545f0498ccd62cf9fac5ed56f76
vicxtt <105440660+vicxtt@users.noreply.github.com>
2024-03-06 08:51:27 -0300

    1_trimetsre_projeto_05_3C

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const botoes = document.querySelectorAll(".botao");
-
+const textos = document.querySelector(".aba-conteudo");
+const contadores = document.querySelector(".contador");
+const tempoObjetivo1 = new Date("2024-03-06T00:00:00");
+let tempoAtual = new Date();
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 for(let i=0;i <botoes.length;i++){ 
     botoes[i].onclick = function() {
 
@@ -7,5 +11,7 @@ for(let i=0;i <botoes.length;i++){
             botoes[j].classList.remove("ativo");
          }
          botoes[i].classList.add("ativo");
+         textos[i].classList.add("ativo");
+         textos[j].classList.remove("ativo");
     }
 }
