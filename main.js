53567910244d6a9839e92ef43d2e4fb0cdc4b6b0
Maria_Clara_Sampaio <127787008+MariaClaraSampaio25@users.noreply.github.com>
2024-04-08 01:23:02 -0300

    teste4

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 
-const tempoObjetivo1 = new Date("2024-03-12T00:00:00");
+const tempoObjetivo1 = new Date("2025-03-12T00:00:00");
 const tempoObjetivo2 = new Date("2024-04-23T00:00:00");
 const tempoObjetivo3 = new Date("2024-04-15T00:00:00");
 const tempoObjetivo4 = new Date("2024-04-12T00:00:00");
